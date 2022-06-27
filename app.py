d6ddd0586f79481441a23ea5d8f7ef00c6946d01
loriussolutions <41658064+loriussolutions@users.noreply.github.com>
2022-06-27 04:36:38 +0200

    final version

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World for 7ASO - v3"
+    return "Hello World for 7ASO - final version !"
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
